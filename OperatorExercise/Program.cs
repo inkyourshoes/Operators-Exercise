3c40e9db52e0878d3fc03c95a1d072de574b36c0
inkyourshoesgit config user.namegit config --global user.email inkyourshoes@gmail.comglobal user.name inkyourshoes <inkyourshoes@gmail.com>
2025-07-14 18:01:05 -0400

    operators done

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,42 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static double AreaOfCircle(double radius)
         {
-            Console.WriteLine("Hello, World!");
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    
+
+    public static void PrintAreaOfCircle()
+        {
+            Console.WriteLine("Enter the radius of the circle");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(
+                $"The area of the circle with a radius of {radius} is {Math.Round(AreaOfCircle(radius), 2)}");
+
+        }
+    static void Main(string[] args)
+        {
+
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int subtraction = a - b;
+            int product = a * b;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            PrintAreaOfCircle();
+            
+            //int i = 3;
+            //int j = 4;
+            //int k = ++i * j++;
+
+            //Console.WriteLine(k); // What will this output?
         }
     }
 }
